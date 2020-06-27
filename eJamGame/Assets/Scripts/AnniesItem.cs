42e6578fe46bffb479943c2f0cff94e0ddd5e194
Annie <anniezhao107@gmail.com>
2020-06-26 23:04:31 -0700

    flowchart variable code block execute

---
+++ eJamGame/Assets/Scripts/AnniesItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class AnniesItem : MonoBehaviour
+{
+    public Flowchart flowchart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        flowchart.SetIntegerVariable("myInt", 2);
+        flowchart.ExecuteBlock("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
