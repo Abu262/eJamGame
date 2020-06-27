ab6ed4e5be0af8c51feda833a2a073414d070768
Annie <anniezhao107@gmail.com>
2020-06-27 15:56:06 -0700

    dialogue box art

--- eJamGame/Assets/Scripts/AnniesItem.cs
+++ eJamGame/Assets/Scripts/AnniesItem.cs
@@ -3,19 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using Fungus;
 
-public class AnniesItem : MonoBehaviour
+public class AnniesItem : ItemClass
 {
     public Flowchart flowchart;
     // Start is called before the first frame update
     void Start()
     {
-        flowchart.SetIntegerVariable("myInt", 2);
-        flowchart.ExecuteBlock("Start");
+        name = "mushroom";
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator test()
+    {
+        if (flowchart.GetFloatVariable("someInt") > 3)
+        {
+        }
+        yield return null;
+    }
+
+    public override IEnumerator Interact()
+    {
+        Debug.Log("This is an override!");
+
+        flowchart.SetIntegerVariable("myInt", 2);
+        flowchart.ExecuteBlock("Start");
+
+        while (flowchart.GetIntegerVariable("myInt") == 2)
+        {
+            yield return null;
+        }
+
+        yield return null;
     }
 }
---
+++ eJamGame/Assets/Scripts/Timer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float timeRemaining = 70f;
+    public bool stillTimeLeft = false;
+    public Text timeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stillTimeLeft = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (stillTimeLeft)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+
+                if (timeRemaining < 30)
+                {
+                    //Trigger some timed event hint
+                }
+            }
+            else
+            {
+                Debug.Log("No more time");
+                timeRemaining = 0;
+                stillTimeLeft = false;
+            }
+        }
+    }
+
+    void DisplayTime(float time)
+    {
+        time += 1;
+        float minutes = Mathf.FloorToInt(time / 60);
+        float seconds = Mathf.FloorToInt(time % 60);
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+}
