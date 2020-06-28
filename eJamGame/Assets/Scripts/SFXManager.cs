41cde9295a2d8ffefd6d507695c34763d1669a26
Jesse <jesseallas@gmail.com>
2020-06-28 11:40:53 -0700

    music and typos

---
+++ eJamGame/Assets/Scripts/SFXManager.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SFXManager : MonoBehaviour
+{
+    public Sound[] soundEffects;
+
+    public static SFXManager instance;
+
+    //Called on initial loading of the game
+    void Awake()
+    {
+        //Start singleton creation
+        if(instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+        //End singleton creation
+
+        //Init sfx
+        foreach(Sound s in soundEffects)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clips[0];
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+            s.source.playOnAwake = false;
+        }
+    }
+
+    public Sound GetSFX(string name)
+    {
+        Sound s = Array.Find(soundEffects, sound => sound.name == name);
+        if (s == null)
+        {
+            if (soundEffects.Length != 0)
+                Debug.LogError(String.Format("Sound cannot be found with name: {0}.", name));
+            else
+                Debug.LogError("Sound Effects List is empty");
+        }
+        return s;
+    }
+
+    public void Play(string name)
+    {
+        Sound s = GetSFX(name);
+        if (s == null)
+            return;
+        s.source.Play();
+    }
+
+    public void PlayWithPitchMod(string name)
+    {
+        Sound s = GetSFX(name);
+        if (s == null)
+            return;
+        s.source.pitch = Random.Range(s.pitchMin, s.pitchMax);
+        s.source.Play();
+    }
+
+    public void PlayWithVolumeMod(string name)
+    {
+        Sound s = GetSFX(name);
+        if (s == null)
+            return;
+        s.source.volume = Random.Range(s.volumeMin, s.volumeMax);
+        s.source.Play();
+    }
+
+    public void PlayWithBothMod(string name)
+    {
+        Sound s = GetSFX(name);
+        if (s == null)
+            return;
+        s.source.pitch = Random.Range(s.pitchMin, s.pitchMax);
+        s.source.volume = Random.Range(s.volumeMin, s.volumeMax);
+        s.source.Play();
+    }
+    public void PlayRandomClip(string name)
+    {
+        Sound s = GetSFX(name);
+        if (s == null)
+            return;
+        s.source.clip = s.clips[Random.Range(0, s.clips.Length)];
+        s.source.Play();
+    }
+    public bool IsPlaying(string name)
+    {
+        Sound s = GetSFX(name);
+        if (s == null)
+            return false;
+        return s.source.isPlaying;
+    }
+}
---
+++ eJamGame/Assets/Scripts/Sound.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+/// <summary>
+/// Wrapper for all things needed for sound
+/// </summary>
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    [Header("AudioSource Values")]
+    [Tooltip("List of audio clips to use for a sound effect")]
+    public AudioClip[] clips;
+    [Tooltip("Default pitch")]
+    [Range(0.0f, 5.0f)]
+    public float pitch = 1.0f;
+    [Tooltip("Default volume")]
+    [Range(0.0f, 1.0f)]
+    public float volume = 1.0f;
+    public bool loop;
+
+    //WARNING: Min < Max or Max > Min should never happen or else
+    //pitch mod WILL throw an error
+    [Header("Pitch Modulator Values")]
+    [Tooltip("Minimum pitch at which the audio source will play in")]
+    [Range(0.0f, 5.0f)]
+    public float pitchMin = 0.0f;
+    [Tooltip("Maximum pitch at which the audio source will play in")]
+    [Range(0.0f, 5.0f)]
+    public float pitchMax = 1.0f;
+
+    //WARNING: Min < Max or Max > Min should never happen or else
+    //volume mod WILL throw an error
+    [Header("Volume Modulator Values")]
+    [Tooltip("Minimum volume at which the audio source will play in")]
+    [Range(0.0f, 1.0f)]
+    public float volumeMin = 0.0f;
+    [Tooltip("Maximum volume at which the audio source will play in")]
+    [Range(0.0f, 1.0f)]
+    public float volumeMax = 1.0f;
+
+    [HideInInspector]
+    public AudioSource source;
+}
