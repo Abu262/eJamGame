ae3dcd960b855d7c6190c546f68bf2519710fb23
Annie <anniezhao107@gmail.com>
2020-06-28 12:41:20 -0700

    prologue scene, edits to build setting

---
+++ eJamGame/Assets/Scripts/PrologueSetUp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+using UnityEngine.SceneManagement;
+
+public class PrologueSetUp : MonoBehaviour
+{
+    public Flowchart flowchart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        flowchart.ExecuteBlock("Start");
+        flowchart.SetBooleanVariable("pDone", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (flowchart.GetBooleanVariable("pDone") == true)
+        {
+            SceneManager.LoadScene(4);
+        }
+    }
+}
